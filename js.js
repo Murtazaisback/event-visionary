88765497e4968a57239b375186a6b8875553feac
Murtazaisback <104103515+Murtazaisback@users.noreply.github.com>
2023-09-30 03:36:29 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -108,8 +108,6 @@ for (let i = 0; i < teamMember.length; i++) {
 
 
 
-
-
 $(function() {
   $("#Sales_tax").on("click",function() {
     $(".tax_inputs").toggle(this.checked);
@@ -118,3 +116,31 @@ $(function() {
 
 
 
+
+
+
+
+
+
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
